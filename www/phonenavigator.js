c465cf4bed00c9ab82b7c69fc75b49ce1624ab00
U-Calipso\vik <vik@Calipso.(none)>
2013-11-02 00:44:19 +0100

    change js file

--- www/phonenavigator.js
+++ www/phonenavigator.js
@@ -1,5 +1,9 @@
-var phonenavigator = {
-    /**
+cordova.define("cordova/plugin/phonenavigator", function(require, exports, module) {
+    var exec = require('cordova/exec');
+    
+    var PhoneNavigator = function() {};
+    
+   /**
      * Opens phone navigator app to navigate to given lat/lon destination
      *
      * @param {Float} lat - destintation latitude as decimal number
@@ -8,14 +12,15 @@ var phonenavigator = {
      * * @param {Function} errorCallback - The callback which will be called when diagnostic of location encounters an error.
      * This callback function have a string param with the error.     
      */
-    doNavigate: function(lat, lon, successCallback, errorCallback) {
-        return cordova.exec(
-            successCallback,
-            errorCallback,
-            'PhoneNavigator',
-            'doNavigate',
-            [lat, lon]
-        );
-    }
-};
-module.exports = phonenavigator;
+    PhoneNavigator.prototype.doNavigate = function(lat, lon, successCallback, errorCallback) {
+        return cordova.exec(successCallback,
+                            errorCallback,
+                            'PhoneNavigator',
+                            'doNavigate',
+                            [lat, lon]);
+    };
+        
+    var phonenavigator = new PhoneNavigator();
+    module.exports = phonenavigator;
+});
+
