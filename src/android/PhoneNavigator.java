dcad34a1d9876f564c916af52cff20b463f313fc
U-Calipso\vik <vik@Calipso.(none)>
2013-11-02 00:24:49 +0100

    Add plugin files

---
+++ src/android/PhoneNavigator.java
@@ -0,0 +1,69 @@
+/**
+ *  Plugin PhoneNavigator
+ *
+ *  Copyright (c) 2013 Dave Alden - Working Edge Ltd.
+ *  
+**/
+
+package org.apache.cordova.plugin;
+
+import org.apache.cordova.api.PluginResult.Status;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
+import org.apache.cordova.api.CallbackContext;
+import org.apache.cordova.api.CordovaInterface;
+import org.apache.cordova.api.CordovaPlugin;
+
+public class PhoneNavigator extends CordovaPlugin {
+
+	private static final String LOG_TAG = "PhoneNavigator";
+
+	
+	@Override
+	public boolean execute(String action, JSONArray args,
+			CallbackContext callbackContext) throws JSONException {
+		boolean result;
+		Log.d(LOG_TAG, "Executing PhoneNavigator Plugin");
+		if ("doNavigate".equals(action)){						
+			result = this.doNavigate(args);
+		} else {
+			Log.d(LOG_TAG, "Invalid action");
+			result = false;
+		}
+		if(result == true){
+			callbackContext.success();
+		}
+		return result;
+	}
+
+	
+	private boolean doNavigate(JSONArray args){
+		boolean result;
+		
+		try {
+			String lat = args.getString(0);
+        	String lon = args.getString(1);
+        	
+        	if (lat != null && lat.length() > 0 && lon != null && lon.length() > 0) {
+        		Log.d(LOG_TAG, "Navigating to lat="+lat+", lon="+lon);
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("google.navigation:q=" + lat +","+ lon)); 
+                this.cordova.getActivity().startActivity(i);
+                result = true;
+            } else {            	
+            	Log.d(LOG_TAG, "Expected two non-empty string arguments for lat and lon." );
+            	result = false;
+            }        	
+		}catch( JSONException e ) {
+			Log.d(LOG_TAG, "Exception occurred: ".concat(e.getMessage()));
+        	result = false;
+		}        
+        return result;
+    }
+
+}
---
+++ www/phonenavigator.js
@@ -0,0 +1,21 @@
+var phonenavigator = {
+    /**
+     * Opens phone navigator app to navigate to given lat/lon destination
+     *
+     * @param {Float} lat - destintation latitude as decimal number
+     * @param {Float} lon - destintation longitude as decimal number 
+     * @param {Function} successCallback - The callback which will be called when diagnostic of location is successful.
+     * * @param {Function} errorCallback - The callback which will be called when diagnostic of location encounters an error.
+     * This callback function have a string param with the error.     
+     */
+    doNavigate: function(lat, lon, successCallback, errorCallback) {
+        return cordova.exec(
+            successCallback,
+            errorCallback,
+            'PhoneNavigator',
+            'doNavigate',
+            [lat, lon]
+        );
+    }
+};
+module.exports = phonenavigator;
